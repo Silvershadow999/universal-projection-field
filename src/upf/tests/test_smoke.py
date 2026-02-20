58381aab4d7c0766351e24d4db571b14fe8f415d
Silvershadow999 <nicole-drinda@hotmail.de>
2026-02-20 13:02:21 +0100

    Update test_smoke.py

--- src/upf/tests/test_smoke.py
+++ src/upf/tests/test_smoke.py
@@ -1,1 +1,6 @@
+from upf.core import UPFConfig, PureUniversalCore
 
+def test_smoke_step():
+    core = PureUniversalCore(UPFConfig(), seed=1)
+    S = core.step(drive=0.2, DOC=0.1)
+    assert len(S) == core.cfg.levels
